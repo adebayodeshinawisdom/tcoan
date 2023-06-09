bacceccba667fdce884305d04eb944670e32602d
Adebayo Wisdom <adebayowisdom796@gmail.com>
2023-06-09 17:58:31 +0100

    header update

--- components/Banner.js
+++ components/Banner.js
@@ -3,16 +3,19 @@ import { Alert } from 'react-bootstrap';
 
 const ScrollListener = () => {
   const [showBanner, setShowBanner] = useState(true);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
 
   useEffect(() => {
     const handleScroll = () => {
-      const scrollPosition = window.pageYOffset;
+      const currentScrollPos = window.pageYOffset;
 
-      if (scrollPosition > 100) {
+      if (currentScrollPos > prevScrollPos) {
         setShowBanner(false);
       } else {
         setShowBanner(true);
       }
+
+      setPrevScrollPos(currentScrollPos);
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -20,11 +23,11 @@ const ScrollListener = () => {
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, []);
+  }, [prevScrollPos]);
 
   return (
     <>
-      {showBanner && <Alert variant="info">You can make your Giving to our bank account directly BANK NAME: FNB. ACCOUNT NUMBER: 62759134035 . ACCOUNT NAME: Tcoan The Temple church of All Nations. SWIFT CODE:Firnzajjxxx. </Alert>}
+      {showBanner && <Alert variant="danger">You can make your Giving to our bank account directly BANK NAME: FNB. ACCOUNT NUMBER: 62759134035 . ACCOUNT NAME: Tcoan The Temple church of All Nations. SWIFT CODE:Firnzajjxxx. </Alert>}
       {/* Your React-Bootstrap component goes here */}
     </>
   );
